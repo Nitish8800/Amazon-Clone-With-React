e3498cd8ba5f93dc172ae09860b96dc33140d284
Nitish8800 <nitishfea120784@gmail.com>
2022-03-24 09:34:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./component/Header";
 import Home from "./component/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header />
+    <Router>
+      <div className="App">
+        {/* Header */}
+        <Header />
 
-      {/* Home */}
-      <Home />
-    </div>
+        {/* Home */}
+        <Home />
+      </div>
+    </Router>
   );
 }
 
